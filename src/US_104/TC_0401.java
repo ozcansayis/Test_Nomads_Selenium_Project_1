293e11285f9c3438d20c390718d8272748a4ad6d
hakanyegin <hakanyeginn@gmail.com>
2024-03-05 02:10:59 +0300

    TC_0402 ve TC_0403 bitti. TC_0401-0402-0403 tamamına yorum satırı eklendi.

--- src/US_104/TC_0401.java
+++ src/US_104/TC_0401.java
@@ -9,10 +9,12 @@ import org.openqa.selenium.WebElement;
 public class TC_0401 extends BaseDriver {
     @Test
     public void TC_401() {
+        //siteye yönlendirildi
         driver.get("https://www.akakce.com/");
-
+        //giriş yap butonuna tıklandı
         WebElement girisYap = driver.findElement(By.linkText("Giriş Yap"));
         girisYap.click();
+        //kullanıcıdan email ve şifresi yazılıp 'Giriş yap' butonuna tıkla
         WebElement email = driver.findElement(By.cssSelector("[id='life']"));
         email.sendKeys("testnomads01@gmail.com");
         WebElement sifre = driver.findElement(By.cssSelector("[id='lifp']"));
--- src/US_104/TC_0402.java
+++ src/US_104/TC_0402.java
@@ -1,4 +1,33 @@
 package US_104;
 
-public class TC_0402 {
+import Utility.BaseDriver;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class TC_0402 extends BaseDriver {
+    @Test
+    public void TC_0402() {
+        //siteye yönlendirildi
+        driver.get("https://www.akakce.com/");
+        //giriş yap butonuna tıklandı
+        WebElement girisYap = driver.findElement(By.linkText("Giriş Yap"));
+        girisYap.click();
+        //geçersiz email ve şifre yazıldı giriş yap butonuna tıklandı
+        WebElement email = driver.findElement(By.cssSelector("[id='life']"));
+        email.sendKeys("testnomads01@gmail.com");
+        WebElement sifre = driver.findElement(By.cssSelector("[id='lifp']"));
+        sifre.sendKeys("Nomad06++");
+        WebElement girisYap2 = driver.findElement(By.cssSelector("[id='lfb']"));
+        girisYap2.click();
+        //hata mesajının kontrolü sağlandı
+        WebElement buton= driver.findElement(By.cssSelector("button[onclick='Modal_v8.close()']"));
+        if (buton.isDisplayed()) {
+            System.out.println("Giriş yapılamadı test başarılı!!!");
+        }
+        else System.out.println("Test başarısız!");
+
+        WaitAndClose();
+    }
 }
+
