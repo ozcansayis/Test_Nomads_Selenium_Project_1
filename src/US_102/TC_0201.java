c1ec5473ddd301a36e2d65db117b33af27e93886
Lenovo <turkogluabdulkabir@gmail.com>
2024-03-04 23:22:03 +0300

    TC_0201 yorum satırları eklendi.

--- src/US_102/TC_0201.java
+++ src/US_102/TC_0201.java
@@ -12,21 +12,29 @@ public class TC_0201 extends BaseDriver {
 
     public void test_2(){
         String Ad = "Test";
+        // Kullanıcı, Akakce.com sitesine tarayıcı üzerinden giriş yapar.
         driver.get("https://www.akakce.com/");
 
+        // Ana sayfada, sağ üst köşede bulunan "Giriş Yap" seçeneğine tıklar
         WebElement GirisYap = driver.findElement(By.xpath("//*[@id='H_rl_v8']/a[2]"));
         GirisYap.click();
 
+        // Kullanıcı, kullanıcı Epostası ve şifresini doğru bir şekilde girer.
         WebElement Eposta = driver.findElement(By.cssSelector("input[id='life']"));
         Eposta.sendKeys("testnomads01@gmail.com");
 
+        // Kullanıcı, kullanıcı şifresini doğru bir şekilde girer.
         WebElement Sifre = driver.findElement(By.cssSelector("input[id='lifp'][type='password']"));
         Sifre.sendKeys("Nomad44++");
 
+        // kullanıcı ekranda gözüken "Giriş Yap" butonuna tıklar.
         driver.findElement(By.cssSelector("input[id='lfb'][type='submit']")).click();
 
+        // Sağ üst köşede kullanıcı adının görüntülendiği alana bakar ve bu
+        //kullanıcı adının, giriş yapılırken girilen kullanıcı adı ile aynı olup olmadığını kontrol eder.
         WebElement ad = driver.findElement(By.xpath("//div[@id='HM_v8']/i/a"));
 
+        //Eğer kullanıcı adı doğru görünüyorsa, hesap doğrulama sürecinin başarıyla tamamlandığını onaylar.
         Assert.assertTrue(" Adlar uymadi", ad.getText().equals(Ad));
 
         WaitAndClose();
